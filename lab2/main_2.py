013c9621ca6d1c239eda87d6427789801dad0ae2
Horqu <pawel.szewczuk.h@gmail.com>
2023-03-12 14:05:22 +0100

    l2e2

---
+++ lab2/main_2.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import Perceptron
+import numpy as np
+import matplotlib.pyplot as plt
+
+fuel = np.loadtxt('fuel.txt')
+X = fuel[:, 0:3]
+Y = fuel[:, 3:4]
+neuron = Perceptron(tol = 1e-3, max_iter = 5)
+neuron.fit(X, Y)
+print('Accuracy' + str(neuron.score(X, Y)))
